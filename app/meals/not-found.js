3a1b8ab1bcfa043b431bdf112d75b98f3806ee26
Dhwanit <dhwanitkapur.dk@gmail.com>
2024-11-14 08:30:56 +0530

    ADD: Initial commit

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal Not Found</h1>
+      <p>Unfortunately, we were not able to find the requested meal.</p>
+    </main>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunately, we cannot service this request right now.</p>
+    </main>
+  );
+}
